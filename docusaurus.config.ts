277e6bfc1fa1efde676e5e55ae0b86673589f2a5
Sanchayan Sarkar <sarkasan@deshaw.com>
2026-01-30 04:11:08 -0500

    chore: configure Docusaurus

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,15 +15,16 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sanchayan-deshaw-personal.github.io',
+  trailingSlash: false, // based on recommendation
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/deploy-docusaurus-to-github-pages/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'sanchayan-deshaw-personal', // Usually your GitHub org/user name.
+  projectName: 'deploy-docusaurus-to-github-pages', // Usually your repo name.
 
   onBrokenLinks: 'throw',
 
