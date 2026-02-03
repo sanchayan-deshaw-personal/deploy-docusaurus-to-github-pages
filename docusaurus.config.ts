61d2d2088583a50a52b02ed8753e8a5749089d9f
Sanchayan Sarkar <sarkasan@deshaw.com>
2026-02-03 10:27:21 -0500

    Updates the editUrls in docusaurus.config.ts for readers to edit

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -46,7 +46,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sanchayan-deshaw-personal/deploy-docusaurus-to-github-pages/tree/main/',
         },
         blog: {
           showReadingTime: true,
@@ -57,7 +57,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/sanchayan-deshaw-personal/deploy-docusaurus-to-github-pages/tree/main/',
           // Useful options to enforce blogging best practices
           onInlineTags: 'warn',
           onInlineAuthors: 'warn',
